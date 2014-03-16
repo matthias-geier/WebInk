6c5fb7fce9c2ac12791577d48423daaa0335294f
Matthias Geier <mayutamano@gmail.com>
2014-03-16 12:28:04 +0100

    Thinning out the unused code from the refactoring

--- lib/webink/database.rb
+++ lib/webink/database.rb
@@ -61,42 +61,14 @@ module Ink
   # A block allows you to assign the k (column name) and v (value) to itm
   # (a type.new taken from the second parameter) however you like.
   #
-  # The following methods are convenience methods to access data for
-  # models. As example a model Apple and its n:1 relation to Tree
-  # are used. Please note that both class and database table name can
-  # be used to call the find and related methods. The table name for
-  # Apple would be "apple"; for MyApple would be "my_apple".
+  #   Ink::Database.database.transaction(false) do
+  #     Ink::R.update(:foobar).set("ref=24").execute
+  #   end
   #
-  #   Ink::Database.database.find "apple", "WHERE id < 10 GROUP BY color"
-  #   => self.query("SELECT * FROM apple WHERE id < 10 GROUP BY color;")
-  #
-  # This is different from the query method, because it returns an Array
-  # of Objects, created by the information stored in the database. So this
-  # find() will return you a set of Apple-instances.
-  #
-  #   Ink::Database.database.find_union "apple", 5, "tree", "AND tree_id>1"
-  #
-  # find_union allows you to retrieve data through a many_many reference.
-  # When you define a many_many relationship, a helper-table is created
-  # inside the database, which is apple_tree, in this case. This statement
-  # will fetch an Array of all Trees, that connect to the Apple with primary
-  # key 5. Notice that the relationship database apple_tree is put together
-  # by the alphabetically first, and then second classname. The last quotes
-  # allow additional query informations to be passed along (like group by)
-  #
-  #   Ink::Database.database.find_references Tree, 1, Apple, "AND tree_id>1"
-  #
-  # find_references is similar to find_union, only that it handles all
-  # other relationships. This statement above requires one Tree to have many
-  # Apples, so it will return an Array of Apples, all those that belong to
-  # the Tree with primary key 1
-  #
-  #   Ink::Database.database.find_reference Apple, 5, Tree, ""
-  #
-  # find_reference is essentially equal to find_references, yet it returns
-  # one result of a Tree or nil. This is used when Apple-Tree is a many_one
-  # or one_one relationship. It saves the need for the result Array from
-  # find_references.
+  # Code can be wrapped in a transaction block and therefore be grouped
+  # or if require rolled back, like in the example above. Giving the
+  # call to #transaction false will automatically roll back the transaction
+  # and giving it true will attempt to commit.
   #
   # Please close the dbinstance once you are done. This is automatically
   # inserted in the init.rb of a project.
@@ -227,115 +199,12 @@ module Ink
 
     # Instance method
     #
-    # Delete something from the database.
-    # [param class_name:] Defines the class name or class
-    # [param params:] Additional SQL syntax like WHERE conditions (optional)
-    def remove(class_name, params="")
-      warn "deprecated #{self.class.name}#remove"
-      @db.remove(class_name, params)
-    end
-
-    # Instance method
-    #
-    # Retrieve class instances, that are loaded with the database result set.
-    # [param class_name:] Defines the class name or class which should be
-    #                     queried
-    # [param params:] Additional SQL syntax like WHERE conditions (optional)
-    # [returns:] Array of class_name instances from the SQL result set
-    def find(class_name, params="")
-      warn "deprecated #{self.class.name}#find"
-      @db.find(class_name, params)
-    end
-
-    # Instance method
-    #
-    # Retrieve class2 instances, that are related to the class1 instance with
-    # primary key class1_id. This is done via an additional relationship table.
-    # Only relevant for many_many relationships.
-    # [param class1:] Reference classname or class
-    # [param class1_id:] Primary key value of the reference classname
-    # [param class2:] Match classname or class
-    # [param params:] Additional SQL syntax like GROUP BY (optional)
-    # [returns:] Array of class2 instances from the SQL result set
-    def find_union(class1, class1_id, class2, params="")
-      warn "deprecated #{self.class.name}#find_union"
-      @db.find_union(class1, class1_id, class2, params)
-    end
-
-    # Instance method
-    #
-    # Retrieve class2 instances, that are related to the class1 instance with
-    # primary key class1_id. Not relevant for many_many relationships
-    # [param class1:] Reference classname or class
-    # [param class1_id:] Primary key value of the reference classname
-    # [param class2:] Match classname or class
-    # [param params:] Additional SQL syntax like GROUP BY (optional)
-    # [returns:] Array of class2 instances from the SQL result set
-    def find_references(class1, class1_id, class2, params="")
-      warn "deprecated #{self.class.name}#find_references"
-      @db.find_references(class1, class1_id, class2, params)
-    end
-
-    # Instance method
-    #
-    # Retrieve one class2 instance, that is related to the class1 instance with
-    # primary key class1_id. Only relevant for one_one and one_many
-    # relationships
-    # [param class1:] Reference classname or class
-    # [param class1_id:] Primary key value of the reference classname
-    # [param class2:] Match classname or class
-    # [param params:] Additional SQL syntax like GROUP BY (optional)
-    # [returns:] single class2 instance from the SQL result set or nil
-    def find_reference(class1, class1_id, class2, params="")
-      warn "deprecated #{self.class.name}#find_reference"
-      @db.find_reference(class1, class1_id, class2, params)
-    end
-
-    # Instance method
-    #
-    # This method attempts to remove all existing relationship data
-    # of instance with link of type: type. For one_one relationships
-    # this works only one way, requiring a second call later on before
-    # setting a new value.
-    # [param instance:] Instance of a class that refers to an existing database entry
-    # [param link:] the related class (not a String, but class reference)
-    # [param type:] relationship type
-    def delete_all_links(instance, link, type)
-      warn "deprecated #{self.class.name}#delete_all_links"
-      @db.delete_all_links(instance, link, type)
-    end
-
-    # Instance method
-    #
-    # Attempt to create links of instance to the data inside value.
-    # link is the class of the related data, and type refers to the
-    # relationship type of the two. When one tries to insert an array
-    # for a x_one relationship, the last entry will be set.
-    # [param instance:] Instance of a class that refers to an existing
-    #                   database entry
-    # [param link:] the related class (not a String, but class reference)
-    # [param type:] relationship type
-    # [param value:] relationship data that was set, either a primary key value,
-    #                or an instance, or an array of both
-    def create_all_links(instance, link, type, value)
-      warn "deprecated #{self.class.name}#create_all_links"
-      @db.create_all_links(instance, link, type, value)
-    end
-
-    # Instance method
-    #
-    # Creates a link between instance and a link with primary fk.
-    # The relationship between the two is defined by type. one_one
-    # relationships are placing an additional call to delete_all_links
-    # that will remove conflicts.
-    # [param instance:] Instance of a class that refers to an existing database
-    #                   entry
-    # [param link:] the related class (not a String, but class reference)
-    # [param type:] relationship type
-    # [param value:] primary key of the relationship, that is to be created
-    def create_link(instance, link, type, fk)
-      warn "deprecated #{self.class.name}#create_link"
-      @db.create_link(instance, link, type, fk)
+    # Wraps the block given into a transaction that can be either set
+    # to commit or rollback.
+    # [param commit:] Commit or rollback; default is commit
+    # [param yield:] Any code block that should be wrapped by a transaction
+    def transaction(commit=true, &blk)
+      @db.transaction(commit, &blk)
     end
 
     # Class method
@@ -345,10 +214,6 @@ module Ink
     # [returns:] Formatted string
     def self.format_date(date)
       @db_class.format_date(date)
-    end
-
-    def transaction(commit=true, &blk)
-      @db.transaction(commit, &blk)
     end
 
   end
