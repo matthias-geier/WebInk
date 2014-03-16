94a2e4dd75ae8bdc8eb7ac52ab20df4f70a23151
Matthias Geier <mayutamano@gmail.com>
2014-03-16 11:10:53 +0100

    Add documentation for r extension

--- lib/webink/extensions/r.rb
+++ lib/webink/extensions/r.rb
@@ -1,6 +1,35 @@
 
 module Ink
   module R
+
+    # = RelationString class extension
+    #
+    # It provides a handy executor mechanic for the webink framework.
+    # Any sql can be built and executed with the usual
+    # Ink::Database.database.query logic, only with less code.
+    #
+    # == Equivalency
+    #
+    #   builder = Ink::R.select('*').from(:foobar)
+    #
+    #   builder.execute
+    #   # equals
+    #   Ink::Database.database.query(builder.to_sql)
+    #
+    # == Hashes
+    #
+    # The default return value of a query is a hash, so the #to_h method
+    # is just an alias for #execute
+    #
+    # == Arrays
+    #
+    # All queries can be reduced into any object, so #to_a is an alias for
+    #
+    #   Ink::Database.database.query(query, Array){ |a, k, v| a << v }
+    #
+    #
+    #
+    #
     class RelationString
       def execute(connection=Ink::Database.database, as=Hash, &blk)
         connection.query(self.to_sql, as, &blk)
